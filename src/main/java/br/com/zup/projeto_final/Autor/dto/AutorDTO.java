c35f2ebc6f6638d333d4daad8a189e19ba843702
Raquel Leão <vc.raquel@gmail.com>
2022-01-21 12:41:39 -0300

    criar classe AutorDTO

--- src/main/java/br/com/zup/projeto_final/Autor/dto/AutorDTO.java
+++ src/main/java/br/com/zup/projeto_final/Autor/dto/AutorDTO.java
@@ -1,2 +1,12 @@
-package br.com.zup.projeto_final.Autor.dto;public class AutorDTO {
+package br.com.zup.projeto_final.Autor.dto;
+
+import lombok.Data;
+
+@Data
+public class AutorDTO {
+    private String nome;
+
+    public AutorDTO(String nome) {
+        this.nome = nome;
+    }
 }
