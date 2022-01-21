34f6c2ca5fd0fee9f85b7ba30e531b4e86be08b1
yan-rds <yyan1207@gmail.com>
2022-01-21 14:19:58 -0300

    Criar metodo gerarToken

--- src/main/java/br/com/zup/projeto_final/seguranca/jwt/JWTComponent.java
+++ src/main/java/br/com/zup/projeto_final/seguranca/jwt/JWTComponent.java
@@ -1,11 +1,26 @@
 package br.com.zup.projeto_final.seguranca.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 
 @Component
 public class JWTComponent {
+    private String segredo = "teste";
+    private Long milissegundo = 600000L;
 
+    public String gerarToken(String username, String id) {
+        Date vencimento = new Date(System.currentTimeMillis() + milissegundo);
 
+        String token = Jwts.builder().setSubject(username)
+                .claim("idUsuario", id).setExpiration(vencimento).claim("aleatorio", "xablau")
+                .signWith(SignatureAlgorithm.HS512, segredo.getBytes()).compact();
+
+        return token;
+
+    }
 
 }
