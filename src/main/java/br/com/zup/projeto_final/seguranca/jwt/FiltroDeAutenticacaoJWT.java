0464c893c1025b8bdc9009d6fabb4b17fa0f9987
yan-rds <yyan1207@gmail.com>
2022-01-21 14:41:55 -0300

    Sobrescrever método AttemptAuthentication

--- src/main/java/br/com/zup/projeto_final/seguranca/jwt/FiltroDeAutenticacaoJWT.java
+++ src/main/java/br/com/zup/projeto_final/seguranca/jwt/FiltroDeAutenticacaoJWT.java
@@ -1,7 +1,18 @@
 package br.com.zup.projeto_final.seguranca.jwt;
 
+import br.com.zup.projeto_final.Usuario.Usuario;
+import br.com.zup.projeto_final.seguranca.jwt.exceptions.AcessoNegadoException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilter {
 
@@ -13,5 +24,20 @@ public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilte
         this.authenticationManager = authenticationManager;
     }
 
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try{
+            Usuario login =  objectMapper.readValue(request.getInputStream(), Usuario.class);
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    login.getEmail(), login.getSenha(), new ArrayList<>()
+            );
+            return  authenticationManager.authenticate(authToken);
+        }catch (IOException e){
+            throw new AcessoNegadoException();
+        }
+    }
 
 }
