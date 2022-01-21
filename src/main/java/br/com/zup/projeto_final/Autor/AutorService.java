341b73b834a7c9ac5fd4c8daddef2de7f90534c7
Raquel Leão <vc.raquel@gmail.com>
2022-01-21 12:45:59 -0300

    criar AutorService

--- src/main/java/br/com/zup/projeto_final/Autor/AutorService.java
+++ src/main/java/br/com/zup/projeto_final/Autor/AutorService.java
@@ -1,2 +1,15 @@
-package br.com.zup.projeto_final.Autor;public class AutorService {
+package br.com.zup.projeto_final.Autor;
+
+import br.com.zup.projeto_final.Autor.customException.AutorNaoEncontradoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AutorService {
+    @Autowired
+    AutorRepository autorRepository;
+
 }
