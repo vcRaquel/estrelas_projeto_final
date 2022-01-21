4aa3db4eca8a01f8f4013c06479b18204424c7f4
yan-rds <yyan1207@gmail.com>
2022-01-21 15:22:17 -0300

    Sobrescrever método doFilterInternal

--- src/main/java/br/com/zup/projeto_final/seguranca/jwt/FiltroDeAutorizacaoJWT.java
+++ src/main/java/br/com/zup/projeto_final/seguranca/jwt/FiltroDeAutorizacaoJWT.java
@@ -2,11 +2,19 @@ package br.com.zup.projeto_final.seguranca.jwt;
 
 import br.com.zup.projeto_final.seguranca.jwt.exceptions.TokenInvalidoException;
 import io.jsonwebtoken.Claims;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
     private JWTComponent jwtComponent;
@@ -28,6 +36,24 @@ public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
         UserDetails usuarioLogado = userDetailsService.loadUserByUsername(claims.getSubject());
 
         return new UsernamePasswordAuthenticationToken(usuarioLogado, null, usuarioLogado.getAuthorities());
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        String token = request.getHeader("Authorization");
+
+        if (token != null && token.startsWith("Token ")) {
+            try {
+                UsernamePasswordAuthenticationToken auth = pegarAutenticacao(token.substring(6));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (TokenInvalidoException exception) {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+            }
+        }
+
+        chain.doFilter(request, response);
     }
 
 }
