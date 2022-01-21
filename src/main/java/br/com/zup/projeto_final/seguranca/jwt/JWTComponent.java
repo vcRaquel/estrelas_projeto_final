4d687fba7f18eb5c92c648bb9bee27ba7ed54720
yan-rds <yyan1207@gmail.com>
2022-01-21 14:21:01 -0300

    Criar método pegarClaims

--- src/main/java/br/com/zup/projeto_final/seguranca/jwt/JWTComponent.java
+++ src/main/java/br/com/zup/projeto_final/seguranca/jwt/JWTComponent.java
@@ -1,5 +1,6 @@
 package br.com.zup.projeto_final.seguranca.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,8 @@ import java.util.Date;
 
 @Component
 public class JWTComponent {
-    private String segredo = "teste";
-    private Long milissegundo = 600000L;
+    private final String segredo = "teste";
+    private final Long milissegundo = 600000L;
 
     public String gerarToken(String username, String id) {
         Date vencimento = new Date(System.currentTimeMillis() + milissegundo);
@@ -20,7 +21,15 @@ public class JWTComponent {
                 .signWith(SignatureAlgorithm.HS512, segredo.getBytes()).compact();
 
         return token;
+    }
 
+    public Claims pegarClaims(String token) {
+        try {
+            Claims claims = Jwts.parser().setSigningKey(segredo.getBytes()).parseClaimsJws(token).getBody();
+            return claims;
+        } catch (Exception e) {
+            throw new TokenInvalidoException();
+        }
     }
 
 }
