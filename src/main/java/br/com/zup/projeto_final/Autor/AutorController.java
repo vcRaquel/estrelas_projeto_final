e3165b1f8d3757b484bfba608fd18bdcabf722c1
Raquel Leão <vc.raquel@gmail.com>
2022-01-21 12:57:58 -0300

    criar AutorController

--- src/main/java/br/com/zup/projeto_final/Autor/AutorController.java
+++ src/main/java/br/com/zup/projeto_final/Autor/AutorController.java
@@ -1,2 +1,24 @@
-package br.com.zup.projeto_final.Autor;public class AutorController {
+package br.com.zup.projeto_final.Autor;
+
+import br.com.zup.projeto_final.Autor.dto.AutorDTO;
+import br.com.zup.projeto_final.Components.ConversorModelMapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/autores") // ver se é plural mesmo
+public class AutorController {
+    @Autowired
+    AutorService autorService;
+    @Autowired
+    ModelMapper modelMapper;
+
+
 }
