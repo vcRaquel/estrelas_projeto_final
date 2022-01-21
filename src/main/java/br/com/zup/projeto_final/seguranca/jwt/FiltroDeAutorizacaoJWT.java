38b74858f3303ba3db91b9741c96ddff7e9d7cbd
yan-rds <yyan1207@gmail.com>
2022-01-21 15:21:46 -0300

    Criar método pegarAutenticacao

--- src/main/java/br/com/zup/projeto_final/seguranca/jwt/FiltroDeAutorizacaoJWT.java
+++ src/main/java/br/com/zup/projeto_final/seguranca/jwt/FiltroDeAutorizacaoJWT.java
@@ -1,6 +1,10 @@
 package br.com.zup.projeto_final.seguranca.jwt;
 
+import br.com.zup.projeto_final.seguranca.jwt.exceptions.TokenInvalidoException;
+import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -13,6 +17,17 @@ public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
         super(authenticationManager);
         this.jwtComponent = jwtComponent;
         this.userDetailsService = userDetailsService;
+    }
+
+    public UsernamePasswordAuthenticationToken pegarAutenticacao(String token){
+        if(!jwtComponent.tokenValido(token)){
+            throw new TokenInvalidoException();
+        }
+
+        Claims claims = jwtComponent.pegarClaims(token);
+        UserDetails usuarioLogado = userDetailsService.loadUserByUsername(claims.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(usuarioLogado, null, usuarioLogado.getAuthorities());
     }
 
 }
