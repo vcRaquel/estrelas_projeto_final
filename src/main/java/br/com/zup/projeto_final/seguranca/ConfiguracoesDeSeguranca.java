f14728343c35e1b7120696adb579214260705b4e
yan-rds <yyan1207@gmail.com>
2022-01-21 12:33:40 -0300

    Criar método configurarCORS

--- src/main/java/br/com/zup/projeto_final/seguranca/ConfiguracoesDeSeguranca.java
+++ src/main/java/br/com/zup/projeto_final/seguranca/ConfiguracoesDeSeguranca.java
@@ -7,8 +7,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,25 +20,12 @@ public class ConfiguracoesDeSeguranca extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    private static final String[] ENDPOINT_POST_PUBLICO = {
-            "/livros",
-            "/usuarios"
-
-    };
 
     @Bean
-    CorsConfigurationSource configurarCORS(){
+    CorsConfigurationSource configurarCORS() {
         UrlBasedCorsConfigurationSource cors = new UrlBasedCorsConfigurationSource();
         cors.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return cors;
     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.cors().configurationSource(configurarCORS());
-
-        http.authorizeHttpRequests()
-                .antMatchers(HttpMethod.POST, "localhost:8080/livros")
-    }
 }
+
---
+++ src/main/java/br/com/zup/projeto_final/seguranca/jwt/JWTComponent.java
@@ -0,0 +1,2 @@
+package br.com.zup.projeto_final.seguranca.jwt;public class JWTComponent {
+}
