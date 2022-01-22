7e02f5c4759dbbfc6e9de21fc06bc1517af4038c
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 18:40:25 -0300

    criar classe UsuarioController

--- src/main/java/br/com/zup/projeto_final/Usuario/UsuarioController.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/UsuarioController.java
@@ -1,2 +1,16 @@
-package br.com.zup.projeto_final.Usuario;public class UsuarioController {
+package br.com.zup.projeto_final.Usuario;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/usuarios")//verificar se é plural mesmo
+public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    ModelMapper modelMapper;
+
 }
