074d2bfeba29815f34b41baf5bfb8d0cacdf1465
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-22 18:22:56 -0300

    Criar exception para livro não encontrado

---
+++ src/main/java/br/com/zup/projeto_final/customException/LivroNaoEncontradoException.java
@@ -0,0 +1,6 @@
+package br.com.zup.projeto_final.customException;
+
+public class LivroNaoEncontradoException extends RuntimeException {
+    public LivroNaoEncontradoException(String mensagem) {
+    }
+}
