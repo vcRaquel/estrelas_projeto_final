3578b77ba1e707e3a18422a4a73ed2c2fd24a9ef
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 18:58:00 -0300

    criar custom exception UsuarioNaoEncontradoException

--- src/main/java/br/com/zup/projeto_final/Usuario/customException/UsuarioNaoEncontradoException.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/customException/UsuarioNaoEncontradoException.java
@@ -1,2 +1,4 @@
-package br.com.zup.projeto_final.Usuario.customException;public class UsuarioNaoEncontradoException {
+package br.com.zup.projeto_final.Usuario.customException;
+
+public class UsuarioNaoEncontradoException extends RuntimeException{
 }
