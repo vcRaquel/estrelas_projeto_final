a159f67137f8ecea3c6d53bb4eee43b5c8225bea
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 19:08:31 -0300

    criar metodo buscarUsuarios UsuarioService

--- src/main/java/br/com/zup/projeto_final/Usuario/UsuarioService.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/UsuarioService.java
@@ -3,6 +3,8 @@ package br.com.zup.projeto_final.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class UsuarioService {
@@ -12,6 +14,11 @@ public class UsuarioService {
     public Usuario salvarUsuario(Usuario usuario) {
         usuarioRepository.save(usuario);
         return usuario;
+    }
+
+    public List<Usuario> buscarUsuarios() {
+        Iterable<Usuario> usuarios = usuarioRepository.findAll();
+        return (List<Usuario>)usuarios ;
     }
 
 }
