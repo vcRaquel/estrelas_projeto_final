2ad1a7233b3b7171427720bb63c60301d901483a
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 18:45:48 -0300

    criar metodo setup UsuarioControllerTest

--- src/test/java/br/com/zup/projeto_final/Usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/projeto_final/Usuario/UsuarioControllerTest.java
@@ -4,6 +4,7 @@ import br.com.zup.projeto_final.Components.ConversorModelMapper;
 import br.com.zup.projeto_final.Usuario.dto.UsuarioDTO;
 import br.com.zup.projeto_final.Usuario.dto.UsuarioSaidaDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,6 +23,26 @@ public class UsuarioControllerTest {
     private Usuario usuario;
     private UsuarioDTO usuarioDTO;
     private UsuarioSaidaDTO usuarioSaidaDTO;
+
+    @BeforeEach
+    public void setup() {
+        usuario = new Usuario();
+        usuario.setId(1);
+        usuario.setEmail("usuario@email.com");
+        usuario.setNome("Zupper");
+        usuario.setSenha("senha123");
+
+        usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setEmail("usuario@email.com");
+        usuarioDTO.setNome("Zupper");
+
+        usuarioSaidaDTO = new UsuarioSaidaDTO();
+        usuarioSaidaDTO.setNome("Zupper");
+        usuarioSaidaDTO.setEmail("usuario@email.com");
+
+
+        objectMapper = new ObjectMapper();
+    }
 
 
 }
