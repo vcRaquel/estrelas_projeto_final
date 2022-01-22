9db0e70b97dce9d82918d1e9b0b8d719ba1e81af
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-22 18:40:12 -0300

    Criar dto para livro

---
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroDTO.java
@@ -0,0 +1,23 @@
+package br.com.zup.projeto_final.Livro;
+
+import br.com.zup.projeto_final.Autor.Autor;
+import br.com.zup.projeto_final.Enun.Genero;
+import br.com.zup.projeto_final.Enun.Tags;
+import br.com.zup.projeto_final.Textos.Comentario;
+import br.com.zup.projeto_final.Textos.Review;
+import lombok.Data;
+
+@Data
+public class LivroDTO {
+
+    public class Livro {
+        private String nome;
+        private Autor autores;
+        private Genero genero;
+        private Tags tags;
+        private Review review;
+        private Comentario comentario;
+        private  boolean lido;
+        private  int curtidas;
+    }
+}
