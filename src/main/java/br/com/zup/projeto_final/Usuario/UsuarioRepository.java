6a46777407889852ba0497752cd0a970edb556e9
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 18:41:21 -0300

    criar interface UsuarioRepository

--- src/main/java/br/com/zup/projeto_final/Usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/UsuarioRepository.java
@@ -1,2 +1,6 @@
-package br.com.zup.projeto_final.Usuario;public interface UsuarioRepository {
+package br.com.zup.projeto_final.Usuario;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
 }
