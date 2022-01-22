f1350c7f99338f66faa984f9dc557e74be637d40
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 19:07:45 -0300

    criar metodo salvarUsuario UsuarioService

--- src/main/java/br/com/zup/projeto_final/Usuario/UsuarioService.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/UsuarioService.java
@@ -9,4 +9,9 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    public Usuario salvarUsuario(Usuario usuario) {
+        usuarioRepository.save(usuario);
+        return usuario;
+    }
+
 }
