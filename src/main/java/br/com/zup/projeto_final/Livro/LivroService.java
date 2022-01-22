0b840b0c68bc4456b8810c6cb6277576d896241a
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-22 18:26:04 -0300

    Criar metodo para atualizar livro

--- src/main/java/br/com/zup/projeto_final/Livro/LivroService.java
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroService.java
@@ -1,9 +1,11 @@
 package br.com.zup.projeto_final.Livro;
 
+import br.com.zup.projeto_final.customException.LivroNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LivroService {
@@ -18,6 +20,18 @@ public class LivroService {
     public List<Livro> buscarLivros (){
         Iterable<Livro> livros = livroRepository.findAll();
         return (List<Livro>) livros;
+    }
+
+    public Livro atualizarLivro (String nome, Livro livro){
+        Optional<Livro> livroOptional = livroRepository.findById(nome);
+
+        if (livroOptional.isEmpty()) {
+            throw new LivroNaoEncontradoException("Livro não cadastrado.");
+        }
+
+        Livro livroParaAtualizar = livroOptional.get();
+        livroRepository.save(livroParaAtualizar);
+        return livroParaAtualizar;
     }
 
 
