330b69ece853c35b2fae2e56dbdce2de81cf07f5
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 18:48:19 -0300

    criar metodo testarCadastroDeUsuario UsuarioControllerTest

--- src/test/java/br/com/zup/projeto_final/Usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/projeto_final/Usuario/UsuarioControllerTest.java
@@ -5,10 +5,16 @@ import br.com.zup.projeto_final.Usuario.dto.UsuarioDTO;
 import br.com.zup.projeto_final.Usuario.dto.UsuarioSaidaDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 @WebMvcTest({UsuarioController.class, ConversorModelMapper.class})
@@ -44,5 +50,17 @@ public class UsuarioControllerTest {
         objectMapper = new ObjectMapper();
     }
 
+    @Test
+    public void testarCadastroDeUsuario() throws Exception {
+        Mockito.when(usuarioService.salvarUsuario(Mockito.any(Usuario.class))).thenReturn(usuario);
+        String json = objectMapper.writeValueAsString(usuarioDTO);
+
+        ResultActions resultado = mockMvc.perform(MockMvcRequestBuilders.post("/usuarios")
+                        .content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect((MockMvcResultMatchers.status().is(201)));
+
+        String jsonResposta = resultado.andReturn().getResponse().getContentAsString();
+        UsuarioSaidaDTO usuarioResposta = objectMapper.readValue(jsonResposta, UsuarioSaidaDTO.class);
+    }
 
 }
