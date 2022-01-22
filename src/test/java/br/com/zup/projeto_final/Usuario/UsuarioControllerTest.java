d0a7e17615ae3940cdfc11002790b7f7db4391c3
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 18:37:37 -0300

    criar UsuarioControllerTest

--- src/test/java/br/com/zup/projeto_final/Usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/projeto_final/Usuario/UsuarioControllerTest.java
@@ -1,2 +1,27 @@
-package br.com.zup.projeto_final.Usuario;public class UsuarioControllerTest {
+package br.com.zup.projeto_final.Usuario;
+
+import br.com.zup.projeto_final.Components.ConversorModelMapper;
+import br.com.zup.projeto_final.Usuario.dto.UsuarioDTO;
+import br.com.zup.projeto_final.Usuario.dto.UsuarioSaidaDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@WebMvcTest({UsuarioController.class, ConversorModelMapper.class})
+public class UsuarioControllerTest {
+    @MockBean
+    UsuarioService usuarioService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+    private Usuario usuario;
+    private UsuarioDTO usuarioDTO;
+    private UsuarioSaidaDTO usuarioSaidaDTO;
+
+
 }
