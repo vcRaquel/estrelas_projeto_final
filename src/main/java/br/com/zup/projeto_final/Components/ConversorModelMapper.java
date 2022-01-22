6dff1b4f370147ef5e12ac37999e99ad7faadce1
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 18:29:29 -0300

    criar conversor para modelmapper

--- src/main/java/br/com/zup/projeto_final/Components/ConversorModelMapper.java
+++ src/main/java/br/com/zup/projeto_final/Components/ConversorModelMapper.java
@@ -1,2 +1,14 @@
-package br.com.zup.projeto_final.Components;public class ConversorModelMapper {
+package br.com.zup.projeto_final.Components;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConversorModelMapper {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
 }
