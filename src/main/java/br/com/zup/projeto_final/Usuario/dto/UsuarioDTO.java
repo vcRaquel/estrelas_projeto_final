4606e2781326c702fe324d0534e1ed78577d90b7
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 18:31:25 -0300

    criar classe UsuarioDTO

--- src/main/java/br/com/zup/projeto_final/Usuario/dto/UsuarioDTO.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/dto/UsuarioDTO.java
@@ -1,2 +1,12 @@
-package br.com.zup.projeto_final.Usuario.dto;public class UsuarioDto {
+package br.com.zup.projeto_final.Usuario.dto;
+
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+    private String nome;
+    private String email;
+    private String senha;
+    //private List<Livro> interesse
+
 }
