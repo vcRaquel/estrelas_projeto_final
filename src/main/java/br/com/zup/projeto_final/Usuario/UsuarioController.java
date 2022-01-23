47915ec6992211adff2660cc4a25c65259cef6f9
yan-rds <yyan1207@gmail.com>
2022-01-23 12:35:13 -0300

    Criar método na controller para pegar ID do usuário logado

--- src/main/java/br/com/zup/projeto_final/Usuario/UsuarioController.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/UsuarioController.java
@@ -1,14 +1,12 @@
 package br.com.zup.projeto_final.Usuario;
-
-import br.com.zup.projeto_final.Usuario.Usuario;
-import br.com.zup.projeto_final.Usuario.UsuarioService;
 import br.com.zup.projeto_final.Usuario.dto.UsuarioDTO;
 import br.com.zup.projeto_final.Usuario.dto.UsuarioSaidaDTO;
+import br.com.zup.projeto_final.seguranca.UsuarioLogado;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,32 +20,42 @@ public class UsuarioController {
     @Autowired
     ModelMapper modelMapper;
 
+
+    private String pegarId() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UsuarioLogado usuarioLogado = (UsuarioLogado) principal;
+        return usuarioLogado.getId();
+    }
+
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UsuarioSaidaDTO cadastrarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO){
+    public UsuarioSaidaDTO cadastrarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) {
         Usuario usuario = modelMapper.map(usuarioDTO, Usuario.class);
         usuarioService.salvarUsuario(usuario);
-
         return modelMapper.map(usuario, UsuarioSaidaDTO.class);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<UsuarioSaidaDTO> exibirUsuarios(){
+    public List<UsuarioSaidaDTO> exibirUsuarios() {
+
         List<UsuarioSaidaDTO> usuarios = new ArrayList<>();
 
-        for (Usuario usuario: usuarioService.buscarUsuarios()){
-            UsuarioSaidaDTO usuarioSaidaDTO = new UsuarioSaidaDTO(usuario.getNome(),usuario.getEmail());
+        for (Usuario usuario : usuarioService.buscarUsuarios()) {
+            UsuarioSaidaDTO usuarioSaidaDTO = new UsuarioSaidaDTO(usuario.getNome(), usuario.getEmail());
             usuarios.add(usuarioSaidaDTO);
         }
+
 
         return usuarios;
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public UsuarioSaidaDTO atualizarUsuario(@PathVariable int id, @RequestBody UsuarioDTO usuarioDTO) throws Exception{
+    public UsuarioSaidaDTO atualizarUsuario(@PathVariable String id, @RequestBody UsuarioDTO usuarioDTO) throws Exception {
         Usuario usuario = usuarioService.atualizarUsuario(id, modelMapper.map(usuarioDTO, Usuario.class));
+
 
         return modelMapper.map(usuario, UsuarioSaidaDTO.class);
 
@@ -55,7 +63,7 @@ public class UsuarioController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletarUsuario(@PathVariable int id){
+    public void deletarUsuario(@PathVariable String id) {
         usuarioService.deletarusuario(id);
     }
 
