0fd904730c1c0a94ba8f3c0652d5a448901cb463
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 22:07:40 -0300

    manipular UsuarioNaoEncontradoException

--- src/main/java/br/com/zup/projeto_final/Config/ControllerAdvice.java
+++ src/main/java/br/com/zup/projeto_final/Config/ControllerAdvice.java
@@ -1,7 +1,17 @@
 package br.com.zup.projeto_final.Config;
+import br.com.zup.projeto_final.Usuario.customException.UsuarioNaoEncontradoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+    @ExceptionHandler(UsuarioNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErroDeValidacao tratarClienteNaoEncontradoException(
+            UsuarioNaoEncontradoException clienteNaoEncontradoException) {
+        return new ErroDeValidacao(clienteNaoEncontradoException.getMessage());
+    }
 
 }
