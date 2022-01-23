4f551bf075be49fc85104a2abacf9627d62a97c8
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 22:02:08 -0300

    adicionar validacoes ao UsuarioDTO

--- src/main/java/br/com/zup/projeto_final/Usuario/dto/UsuarioDTO.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/dto/UsuarioDTO.java
@@ -2,10 +2,19 @@ package br.com.zup.projeto_final.Usuario.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class UsuarioDTO {
+    @NotBlank
+    @Size(min = 2,message = "O nome não pode ter menos de 2 caracteres")
     private String nome;
+    @Email
     private String email;
+    @NotNull
     private String senha;
     //private List<Livro> interesse
 
