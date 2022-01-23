7901599ce92e98f45006bed68117891d11d1aa6b
Raquel Leão <vc.raquel@gmail.com>
2022-01-22 22:09:35 -0300

    manipular MethodArgumentNotValidException

--- src/main/java/br/com/zup/projeto_final/Config/ControllerAdvice.java
+++ src/main/java/br/com/zup/projeto_final/Config/ControllerAdvice.java
@@ -1,9 +1,14 @@
 package br.com.zup.projeto_final.Config;
 import br.com.zup.projeto_final.Usuario.customException.UsuarioNaoEncontradoException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvice {
@@ -12,6 +17,21 @@ public class ControllerAdvice {
     public ErroDeValidacao tratarClienteNaoEncontradoException(
             UsuarioNaoEncontradoException clienteNaoEncontradoException) {
         return new ErroDeValidacao(clienteNaoEncontradoException.getMessage());
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ErroDeValidacao> tratarErrosDeValidacao(MethodArgumentNotValidException excecao) {
+        List<ErroDeValidacao> erros = new ArrayList<>();
+
+        for (FieldError referencia : excecao.getFieldErrors()) {
+            ErroDeValidacao mensagem = new ErroDeValidacao(referencia.getDefaultMessage());
+            erros.add(mensagem);
+        }
+
+        return erros;
+
     }
 
 }
