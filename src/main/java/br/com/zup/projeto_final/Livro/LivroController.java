4f3502799ebc84d768ad9c74a2296d58eacbcd6e
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-24 09:49:51 -0300

    Implementar método que exibe a lista de livros

--- src/main/java/br/com/zup/projeto_final/Livro/LivroController.java
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
@@ -11,11 +14,23 @@ public class LivroController {
     LivroService livroService;
 
     @PostMapping
-    @ResponseStatus (HttpStatus.CREATED)
-    public LivroDTO cadastrarLivro (@RequestBody Livro livroDTO) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public LivroDTO cadastrarLivro(@RequestBody Livro livroDTO) {
         Livro livro = modelMapper.map(livroDTO, Livro.class);
         livroService.salvarLivro(livro);
         return livroDTO;
     }
+
+    public List<LivroDTO> exibirLivros() {
+        List<LivroDTO> livrosDTO = new ArrayList<>();
+
+        for (Livro livro : livroService.buscarLivros()) {
+            LivroDTO livroDTO = new LivroDTO(livro.getNome());
+            livrosDTO.add(livroDTO);
+        }
+
+        return livrosDTO;
+    }
+
 
 }
