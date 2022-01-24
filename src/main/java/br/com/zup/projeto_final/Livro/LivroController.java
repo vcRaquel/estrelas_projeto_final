f309719faf4f79296a38ff6b1bdce431a913b545
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-24 09:45:02 -0300

    Criar metodo que cadastra um livro

--- src/main/java/br/com/zup/projeto_final/Livro/LivroController.java
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroController.java
@@ -1,12 +1,21 @@
 package br.com.zup.projeto_final.Livro;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
     @Autowired
     LivroService livroService;
+
+    @PostMapping
+    @ResponseStatus (HttpStatus.CREATED)
+    public LivroDTO cadastrarLivro (@RequestBody Livro livroDTO) {
+        Livro livro = modelMapper.map(livroDTO, Livro.class);
+        livroService.salvarLivro(livro);
+        return livroDTO;
+    }
+
 }
