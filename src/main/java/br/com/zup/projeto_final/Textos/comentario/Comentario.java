6b12148086379ab7d1b7c349ab7963420b3d2db1
yan-rds <yyan1207@gmail.com>
2022-01-25 19:50:12 -0300

    Implementar relações na Model comentário

--- src/main/java/br/com/zup/projeto_final/Textos/comentario/Comentario.java
+++ src/main/java/br/com/zup/projeto_final/Textos/comentario/Comentario.java
@@ -1,5 +1,6 @@
-package br.com.zup.projeto_final.Textos;
+package br.com.zup.projeto_final.Textos.comentario;
 
+import br.com.zup.projeto_final.Usuario.Usuario;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -11,6 +12,9 @@ public class Comentario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private int idLivro;
+    @OneToOne
+    private Usuario quemComentou;
     private String texto;
 
 
