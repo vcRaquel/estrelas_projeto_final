3ec39e01fb3d9b40d3b0df9536bab83bea0a7dce
yan-rds <yyan1207@gmail.com>
2022-01-25 19:49:23 -0300

    Criar método atualizarComentariosDoLivro

--- src/main/java/br/com/zup/projeto_final/Livro/LivroService.java
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroService.java
@@ -1,6 +1,9 @@
 package br.com.zup.projeto_final.Livro;
 
+import br.com.zup.projeto_final.Textos.comentario.Comentario;
+import br.com.zup.projeto_final.Usuario.Usuario;
 import br.com.zup.projeto_final.Usuario.UsuarioService;
+import br.com.zup.projeto_final.Usuario.customException.UsuarioNaoEncontradoException;
 import br.com.zup.projeto_final.customException.LivroNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +27,15 @@ public class LivroService {
     public List<Livro> buscarLivros() {
         Iterable<Livro> livros = livroRepository.findAll();
         return (List<Livro>) livros;
+    }
+
+    public void atualizarComentariosDoLivro(int idLivro, Comentario comentario){
+        Optional<Livro> livroOptional = livroRepository.findById(idLivro);
+        if (livroOptional.isEmpty()){
+            throw new LivroNaoEncontradoException("Livro não encontrado");
+        }
+        livroOptional.get().getComentarios().add(comentario);
+        livroRepository.save(livroOptional.get());
     }
 
     public Livro atualizarLivro(int id, Livro livro) {
