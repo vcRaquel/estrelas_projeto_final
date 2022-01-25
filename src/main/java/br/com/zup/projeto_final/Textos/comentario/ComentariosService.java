320a3227df7b87926fe9fd16594c5546b4109669
yan-rds <yyan1207@gmail.com>
2022-01-25 19:30:47 -0300

    Criar método salvarComentario

--- src/main/java/br/com/zup/projeto_final/Textos/comentario/ComentariosService.java
+++ src/main/java/br/com/zup/projeto_final/Textos/comentario/ComentariosService.java
@@ -1,4 +1,30 @@
 package br.com.zup.projeto_final.Textos.comentario;
 
+import br.com.zup.projeto_final.Usuario.Usuario;
+import br.com.zup.projeto_final.Usuario.UsuarioRepository;
+import br.com.zup.projeto_final.Usuario.UsuarioService;
+import br.com.zup.projeto_final.Usuario.customException.UsuarioNaoEncontradoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ComentariosService {
+
+    @Autowired
+    ComentarioRepository comentarioRepository;
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public void salvarComentario(String id, Comentario comentario){
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (usuario.isEmpty()){
+            throw new UsuarioNaoEncontradoException();
+        }
+        comentario.setQuemComentou(usuario.get());
+        comentarioRepository.save(comentario);
+    }
+
+
 }
