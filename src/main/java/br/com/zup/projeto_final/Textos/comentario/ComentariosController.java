f5b2e1851c62bd45a91b0735b582a6619e25e5c6
yan-rds <yyan1207@gmail.com>
2022-01-25 19:50:47 -0300

    Criar método cadastrarComentário

--- src/main/java/br/com/zup/projeto_final/Textos/comentario/ComentariosController.java
+++ src/main/java/br/com/zup/projeto_final/Textos/comentario/ComentariosController.java
@@ -1,13 +1,16 @@
 package br.com.zup.projeto_final.Textos.comentario;
 
+import br.com.zup.projeto_final.Livro.Livro;
+import br.com.zup.projeto_final.Livro.LivroDTO;
+import br.com.zup.projeto_final.Textos.comentario.dtos.ComentarioDTO;
 import br.com.zup.projeto_final.Usuario.UsuarioController;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/Comentarios")
+@RequestMapping("/comentarios")
 public class ComentariosController {
 
     @Autowired
@@ -17,6 +20,12 @@ public class ComentariosController {
     @Autowired
     ModelMapper modelMapper;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarComentario(@RequestBody ComentarioDTO comentarioDTO) {
+        Comentario comentario = modelMapper.map(comentarioDTO, Comentario.class);
+        comentariosService.salvarComentario(usuarioController.pegarId(), comentario.getIdLivro(), comentario);
 
+    }
 
 }
