58b0459fb3d63f64fc5c37d5072c5c589f68d4aa
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-26 14:21:33 -0300

    Criar lista para filtro genero na repository

--- src/main/java/br/com/zup/projeto_final/Livro/LivroRepository.java
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroRepository.java
@@ -1,6 +1,11 @@
 package br.com.zup.projeto_final.Livro;
 
+import br.com.zup.projeto_final.Enun.Genero;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LivroRepository extends CrudRepository <Livro, Integer> {
+    List <Livro> findAllByGenero (Genero genero);
+
 }
