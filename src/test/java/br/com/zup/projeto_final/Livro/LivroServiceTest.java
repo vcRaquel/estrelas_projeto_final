9f0b7c1c5bff0e2e05bee1c887fa065fef79aa86
Raquel Leão <vc.raquel@gmail.com>
2022-01-26 16:44:14 -0300

    criar metodo setup

--- src/test/java/br/com/zup/projeto_final/Livro/LivroServiceTest.java
+++ src/test/java/br/com/zup/projeto_final/Livro/LivroServiceTest.java
@@ -1,5 +1,8 @@
 package br.com.zup.projeto_final.Livro;
 
+import br.com.zup.projeto_final.Enun.Genero;
+import br.com.zup.projeto_final.Enun.Tags;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,4 +17,15 @@ public class LivroServiceTest {
 
     private Livro livro;
 
+    @BeforeEach
+    public void setup(){
+        livro = new Livro();
+        livro.setNome("Livro");
+        livro.setGenero(Genero.AVENTURA);
+        livro.setId(1);
+        livro.setLido(true);
+        livro.setCurtidas(1);
+        livro.setTags(Tags.LEITURA_LEVE);
+
+    }
 }
