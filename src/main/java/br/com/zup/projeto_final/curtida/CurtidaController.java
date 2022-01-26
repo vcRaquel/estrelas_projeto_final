50b750e475416b52293d6468f6c25316c2c43073
yan-rds <yyan1207@gmail.com>
2022-01-26 18:45:50 -0300

    Criar CurtidaController

--- src/main/java/br/com/zup/projeto_final/curtida/CurtidaController.java
+++ src/main/java/br/com/zup/projeto_final/curtida/CurtidaController.java
@@ -1,2 +1,17 @@
-package br.com.zup.projeto_final.curtida;public class CurtidaController {
+package br.com.zup.projeto_final.curtida;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/curtida")
+public class CurtidaController {
+
+    @Autowired
+    CurtidaService curtidaService;
+    @Autowired
+    ModelMapper modelMapper;
+
 }
