7c97fc64989b3b80bb0bdced626ea9600b0e18f5
yan-rds <yyan1207@gmail.com>
2022-01-26 18:50:33 -0300

    Criar model Curtida

--- src/main/java/br/com/zup/projeto_final/curtida/Curtida.java
+++ src/main/java/br/com/zup/projeto_final/curtida/Curtida.java
@@ -1,2 +1,21 @@
-package br.com.zup.projeto_final.curtida;public class Curtida {
+package br.com.zup.projeto_final.curtida;
+
+
+import br.com.zup.projeto_final.Enun.Tipo;
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+public class Curtida {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int id_recurso;
+    private int id_usuario;
+    private Tipo tipo;
+
 }
