2276a8b2454bf1faa8173c3be18728968869852a
yan-rds <yyan1207@gmail.com>
2022-01-26 19:06:38 -0300

    Criar método salvarCurtida na Service

--- src/main/java/br/com/zup/projeto_final/curtida/CurtidaService.java
+++ src/main/java/br/com/zup/projeto_final/curtida/CurtidaService.java
@@ -1,7 +1,15 @@
 package br.com.zup.projeto_final.curtida;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurtidaService {
+
+    @Autowired
+    CurtidaRepository curtidaRepository;
+
+    public void salvarCurtida(Curtida curtida){
+        curtidaRepository.save(curtida);
+    }
 }
