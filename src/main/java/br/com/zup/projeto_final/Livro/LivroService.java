38b089fe2d63d33ecc5658c887d179cf812319c1
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-26 14:39:12 -0300

    Criar metodo que exibi todos os livros

--- src/main/java/br/com/zup/projeto_final/Livro/LivroService.java
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroService.java
@@ -1,9 +1,7 @@
 package br.com.zup.projeto_final.Livro;
-
 import br.com.zup.projeto_final.Textos.comentario.Comentario;
-import br.com.zup.projeto_final.Usuario.Usuario;
 import br.com.zup.projeto_final.Usuario.UsuarioService;
-import br.com.zup.projeto_final.Usuario.customException.UsuarioNaoEncontradoException;
+import br.com.zup.projeto_final.Enun.Genero;
 import br.com.zup.projeto_final.customException.LivroNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,13 +27,19 @@ public class LivroService {
         return (List<Livro>) livros;
     }
 
-    public void atualizarComentariosDoLivro(int idLivro, Comentario comentario){
+    public void atualizarComentariosDoLivro(int idLivro, Comentario comentario) {
         Optional<Livro> livroOptional = livroRepository.findById(idLivro);
-        if (livroOptional.isEmpty()){
+        if (livroOptional.isEmpty()) {
             throw new LivroNaoEncontradoException("Livro não encontrado");
         }
         livroOptional.get().getComentarios().add(comentario);
         livroRepository.save(livroOptional.get());
+    }
+
+    public List<Livro> exibirTodosOsLivros(Genero genero) {
+        List<Livro> livros = (List<Livro>) livroRepository.findAll();
+        return livros;
+
     }
 
     public Livro atualizarLivro(int id, Livro livro) {
