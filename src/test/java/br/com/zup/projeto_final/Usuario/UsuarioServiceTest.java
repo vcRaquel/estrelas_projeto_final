eb20f635fa88e88a4ee06a7587e82a533324537f
Raquel Leão <vc.raquel@gmail.com>
2022-01-26 10:28:39 -0300

    criar metodo setup em UsuarioServiceTest

--- src/test/java/br/com/zup/projeto_final/Usuario/UsuarioServiceTest.java
+++ src/test/java/br/com/zup/projeto_final/Usuario/UsuarioServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.zup.projeto_final.Usuario;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,5 +11,17 @@ public class UsuarioServiceTest {
     UsuarioRepository usuarioRepository;
     @Autowired
     UsuarioService usuarioService;
+
+    private Usuario usuario;
+
+    @BeforeEach
+    public void setup() {
+        usuario = new Usuario();
+        usuario.setId("1");
+        usuario.setEmail("zupper@zup.com");
+        usuario.setNome("Zupper");
+        usuario.setSenha("aviao11");
+
+    }
 
 }
