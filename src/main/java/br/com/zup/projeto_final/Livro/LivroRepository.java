7a9979bef5033bb9914d946f44a9d55c813e31ad
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-26 22:00:44 -0300

    Criar lista para filtro tags

--- src/main/java/br/com/zup/projeto_final/Livro/LivroRepository.java
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroRepository.java
@@ -1,11 +1,13 @@
 package br.com.zup.projeto_final.Livro;
 
 import br.com.zup.projeto_final.Enun.Genero;
+import br.com.zup.projeto_final.Enun.Tags;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface LivroRepository extends CrudRepository <Livro, Integer> {
     List <Livro> findAllByGenero (Genero genero);
+    List<Livro> findAllByTags (Tags tags);
 
 }
