6695975d75dfaeccf809f7cd36e237ce504d729b
yan-rds <yyan1207@gmail.com>
2022-01-27 16:04:34 -0300

    Criar CurtidaRepetidaException

---
+++ src/main/java/br/com/zup/projeto_final/customException/CurtidaRepetidaException.java
@@ -0,0 +1,7 @@
+package br.com.zup.projeto_final.customException;
+
+public class CurtidaRepetidaException extends RuntimeException{
+    public CurtidaRepetidaException(String message) {
+        super(message);
+    }
+}
