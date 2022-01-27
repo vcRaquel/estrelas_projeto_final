5e56e367347a653d5717c4b77b52be96c31c883e
Michelle Kênia <keniamsoaress@gmail.com>
2022-01-27 14:19:04 -0300

    Criar lista de nomes na repository

--- src/main/java/br/com/zup/projeto_final/Livro/LivroRepository.java
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroRepository.java
@@ -4,10 +4,12 @@ import br.com.zup.projeto_final.Enun.Genero;
 import br.com.zup.projeto_final.Enun.Tags;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.naming.Name;
 import java.util.List;
 
 public interface LivroRepository extends CrudRepository <Livro, Integer> {
     List <Livro> findAllByGenero (Genero genero);
     List<Livro> findAllByTags (Tags tags);
+    List<Livro> findAllByNome(String nome);
 
 }
