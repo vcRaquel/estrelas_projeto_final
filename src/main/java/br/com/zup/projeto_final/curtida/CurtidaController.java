2dac7d21f7d01ff6e5b60671f8f72e08b2da7637
yan-rds <yyan1207@gmail.com>
2022-01-27 16:41:04 -0300

    Criar método salvarCurtida

--- src/main/java/br/com/zup/projeto_final/curtida/CurtidaController.java
+++ src/main/java/br/com/zup/projeto_final/curtida/CurtidaController.java
@@ -1,9 +1,10 @@
 package br.com.zup.projeto_final.curtida;
 
+import br.com.zup.projeto_final.curtida.dtos.CurtidaEntradaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/curtida")
@@ -14,4 +15,12 @@ public class CurtidaController {
     @Autowired
     ModelMapper modelMapper;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void salvarCurtida (@RequestBody CurtidaEntradaDTO curtidaEntradaDTO){
+
+        Curtida curtida = modelMapper.map(curtidaEntradaDTO, Curtida.class);
+        curtidaService.salvarCurtida(curtida);
+
+    }
 }
