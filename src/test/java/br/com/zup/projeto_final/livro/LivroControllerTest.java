5c82af7305bdc4ce009ea33d5f81bd800ecca6a4
yan-rds <yyan1207@gmail.com>
2022-01-29 11:59:19 -0300

    Criar classe UsuarioLogadoTest

---
+++ src/test/java/br/com/zup/projeto_final/livro/LivroControllerTest.java
@@ -0,0 +1,2 @@
+package br.com.zup.projeto_final.livro;public class LivroControllerTest {
+}
---
+++ src/test/java/br/com/zup/projeto_final/usuarioLogado/UsuarioLogadoServiceTest.java
@@ -0,0 +1,12 @@
+package br.com.zup.projeto_final.usuarioLogado;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UsuarioLogadoServiceTest {
+
+
+
+
+
+}
