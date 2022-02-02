eb85765b1c99caa2e3509a09831714f3aa8ba228
yan-rds <yyan1207@gmail.com>
2022-02-02 17:15:01 -0300

    Possibilitar visualização de comentários - Controller

--- src/main/java/br/com/zup/projeto_final/Textos/comentario/ComentariosController.java
+++ src/main/java/br/com/zup/projeto_final/Textos/comentario/ComentariosController.java
@@ -1,14 +1,15 @@
 package br.com.zup.projeto_final.Textos.comentario;
 
-import br.com.zup.projeto_final.Livro.Livro;
-import br.com.zup.projeto_final.Livro.LivroDTO;
+
 import br.com.zup.projeto_final.Textos.comentario.dtos.ComentarioDTO;
-import br.com.zup.projeto_final.Usuario.UsuarioController;
 import br.com.zup.projeto_final.usuarioLogado.UsuarioLogadoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/comentarios")
@@ -27,6 +28,24 @@ public class ComentariosController {
         Comentario comentario = modelMapper.map(comentarioDTO, Comentario.class);
         comentariosService.salvarComentario(usuarioLogadoService.pegarId(), comentario);
 
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void exibirUsuario(@PathVariable int id) {
+        Comentario usuario  = comentariosService.buscarComentario(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ComentarioDTO> exibirUsuarios() {
+
+        List<ComentarioDTO> comentarios = new ArrayList<>();
+        for (Comentario comentario : comentariosService.buscarComentarios()) {
+            ComentarioDTO comentarioDTO = new ComentarioDTO(comentario.getTexto(), comentario.getLivro_id());
+            comentarios.add(comentarioDTO);
+        }
+        return comentarios;
     }
 
 }
--- src/main/java/br/com/zup/projeto_final/Textos/comentario/dtos/ComentarioDTO.java
+++ src/main/java/br/com/zup/projeto_final/Textos/comentario/dtos/ComentarioDTO.java
@@ -1,8 +1,10 @@
 package br.com.zup.projeto_final.Textos.comentario.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ComentarioDTO {
 
     private String texto;
