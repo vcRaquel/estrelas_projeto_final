234e6ddc766b5d68523b4d2e2750eef6a36e9014
yan-rds <yyan1207@gmail.com>
2022-02-06 18:33:11 -0300

    Refatorar Repository para paginação

--- src/main/java/br/com/zup/projeto_final/Livro/LivroRepository.java
+++ src/main/java/br/com/zup/projeto_final/Livro/LivroRepository.java
@@ -1,27 +1,34 @@
 package br.com.zup.projeto_final.Livro;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface LivroRepository extends CrudRepository <Livro, Integer> {
+public interface LivroRepository extends PagingAndSortingRepository <Livro, Integer> {
 
     @Query(value = "select * from livros l WHERE l.genero like %?1% AND l.tags like %?2% AND l.nome like %?3% AND l.autor like %?4%",
             nativeQuery = true)
-    List <Livro> aplicarTodosFiltros(String genero, String tags, String nome, String autor);
+    Page<Livro> aplicarTodosFiltros(String genero, String tags, String nome, String autor, Pageable pageable);
 
     @Query(value = "select * from livros l WHERE l.genero = ?1 AND l.nome like %?2% AND l.autor like %?3%",
             nativeQuery = true)
-    List <Livro> aplicarFiltroGenero(String genero, String nome, String autor);
+   Page <Livro> aplicarFiltroGenero(String genero, String nome, String autor, Pageable pageable);
 
     @Query(value = "select * from livros l WHERE l.tags like ?1 AND l.nome like %?2% AND l.autor like %?3%",
             nativeQuery = true)
-    List <Livro> aplicarFiltroTags(String tags, String nome, String autor);
+    Page <Livro> aplicarFiltroTags(String tags, String nome, String autor, Pageable pageable);
 
     @Query(value = "select * from livros l WHERE l.nome like %?1% AND l.autor like %?2%",
             nativeQuery = true)
-    List<Livro> aplicarFiltroNomeEAutor(String nome, String autor);
+    Page<Livro> aplicarFiltroNomeEAutor(String nome, String autor, Pageable pageable);
+
+    @Override
+    Page<Livro> findAll(Pageable pageable);
 
     @Query(value = "select * from livros  WHERE nome_tratado = :nome AND autor_tratado = :autor",
             nativeQuery = true)
