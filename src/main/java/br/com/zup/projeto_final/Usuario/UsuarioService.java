d687b44b4a4bbb521e0be79a0206f1beef83a325
yan-rds <yyan1207@gmail.com>
2022-02-07 19:31:02 -0300

    Implementar co-relação de filtros na service

--- src/main/java/br/com/zup/projeto_final/Usuario/UsuarioService.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/UsuarioService.java
@@ -34,21 +34,22 @@ public class UsuarioService {
 
     }
 
-    public List<Usuario> aplicarFiltroUsuario(String nomeUsuario) {
+
+
+    public List<Usuario> buscarUsuarios(String nomeUsuario, boolean orderByPontuacao) {
         List<Usuario> usuarios = new ArrayList<>();
-        if (nomeUsuario != null) {
+
+        if (nomeUsuario == null & !orderByPontuacao){
+            usuarios = (List<Usuario>) usuarioRepository.findAll();
+        }
+        else if (nomeUsuario == null){
+            usuarios = usuarioRepository.findAllByOrderByPontuacaoDesc();
+        }
+        else if (!orderByPontuacao){
             usuarios = usuarioRepository.aplicarFiltroNome(nomeUsuario);
         }
-        return usuarios;
-
-    }
-
-    public List<Usuario> buscarUsuarios(String nomeUsuario) {
-        List<Usuario> usuarios = new ArrayList<>();
-        usuarios = aplicarFiltroUsuario(nomeUsuario);
-        if (nomeUsuario == null){
-            usuarios = (List<Usuario>) usuarioRepository.findAll();
-
+        else{
+            usuarios = usuarioRepository.aplicarFiltroNomeByOrderByPontuacaoDesc(nomeUsuario);
         }
         return usuarios;
 
