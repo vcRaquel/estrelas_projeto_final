2eb72ae628e0e084a9abc7d849df389c6ad21c1b
yan-rds <yyan1207@gmail.com>
2022-02-07 19:30:06 -0300

    Adicionar Pontuacao ao usuarioSaidaDTO

--- src/main/java/br/com/zup/projeto_final/Usuario/dto/UsuarioSaidaDTO.java
+++ src/main/java/br/com/zup/projeto_final/Usuario/dto/UsuarioSaidaDTO.java
@@ -1,20 +1,19 @@
 package br.com.zup.projeto_final.Usuario.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UsuarioSaidaDTO {
     private String nome;
     private String email;
+    private int pontuacao;
     //private List<Livro> interesse
 
     public UsuarioSaidaDTO() {
 
     }
 
-    public UsuarioSaidaDTO(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
-    }
 
 }
