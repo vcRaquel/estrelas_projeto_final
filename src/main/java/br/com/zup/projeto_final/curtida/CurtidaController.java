851a0af68b061a4454be31c808fb43b16140cd0d
Raquel Leão <vc.raquel@gmail.com>
2022-02-09 09:36:43 -0300

    Adicionar configuração do swagger às rotas de curtida

--- src/main/java/br/com/zup/projeto_final/curtida/CurtidaController.java
+++ src/main/java/br/com/zup/projeto_final/curtida/CurtidaController.java
@@ -1,6 +1,8 @@
 package br.com.zup.projeto_final.curtida;
 
 import br.com.zup.projeto_final.curtida.dtos.CurtidaEntradaDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/curtida")
+@Api(value = "Clube de leitura")
+@CrossOrigin(origins = "*")
 public class CurtidaController {
 
     @Autowired
@@ -16,6 +20,7 @@ public class CurtidaController {
     ModelMapper modelMapper;
 
     @PostMapping
+    @ApiOperation(value = "Salvar Curtida")
     @ResponseStatus(HttpStatus.CREATED)
     public void salvarCurtida (@RequestBody CurtidaEntradaDTO curtidaEntradaDTO){
 
