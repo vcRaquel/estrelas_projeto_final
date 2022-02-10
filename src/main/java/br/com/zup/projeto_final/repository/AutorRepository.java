d1fa93d2d2862405225229156a24e006403908d1
yan-rds <yyan1207@gmail.com>
2022-02-10 15:15:56 -0300

    Remover Autor do Swagger

--- src/main/java/br/com/zup/projeto_final/repository/AutorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.zup.projeto_final.repository;
-
-import br.com.zup.projeto_final.model.Autor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AutorRepository extends CrudRepository <Autor, Integer>{
-}
